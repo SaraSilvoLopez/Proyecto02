1e7b796949327b88eac178c3a5e293bb0285a265
unknown <you@example.com>
2021-06-24 12:06:11 +0200

    primera actualizacion

--- Proyecto02/src/main/java/com/example/demo/dao/JuegoDao.java
+++ Proyecto02/src/main/java/com/example/demo/dao/JuegoDao.java
@@ -1,5 +1,25 @@
 package com.example.demo.dao;
 
-public interface JuegoDao {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.Juego;
+
+/**
+ * @ClassName JuegoDao
+ *
+ * @author María Castro
+ * @date 24 jun. 2021
+ * 
+ * @version 1.0
+ */
+@Repository
+public interface JuegoDao extends JpaRepository<Juego,Integer> {
+	
+	@Query("FROM Juego WHERE genero='Platform'")
+	List<Juego> findByGeneroPlataforma();
+	
 }
--- Proyecto02/src/main/java/com/example/demo/service/JuegoService.java
+++ Proyecto02/src/main/java/com/example/demo/service/JuegoService.java
@@ -1,5 +1,33 @@
 package com.example.demo.service;
 
+import java.util.List;
+
+import com.example.demo.model.Juego;
+
+/**
+ * @ClassName JuegoService
+ *
+ * @author María Castro
+ *
+ * @date 24 jun. 2021
+ * 
+ * @version 1.0
+ */
 public interface JuegoService {
+	
+	//Para modificar y añadir juego
+	public void save(Juego juego);
+	
+	//Para recuperar un juego y poder modificarlo
+	public Juego getById(int id);
+	
+	//Para eliminar juego por id
+	public void deleteById(int id);
+	
+	//Para listar todos los juegos
+	public List<Juego> findAll();
+	
+	//Merodo propio para listar juegos de genero = plataforma
+	public List<Juego> findByGeneroPlataforma();
 
 }
